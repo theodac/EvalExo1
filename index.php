f9bf8ef8457fca2d602067458c576b91dbd305cb
theodac <32874437+theodac@users.noreply.github.com>
2018-02-09 13:29:33 +0100

    Ajout de l'exercice terminé

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$tableau = [
+    "Prenom" => "Théo",
+    "Nom" => "Da Costa",
+    "Adresse" => "Rue du bec",
+    "Code Postale" => "76000",
+    "Ville" => "Rouen",
+    "Email" => "theo.dacos@gmail.com",
+    "Téléphone" => "06.67.72.04.19",
+    "Date de naissance" => "13/11/1997"
+];
+foreach ($tableau as $valeur){
+
+    echo   $valeur ." <br>";
+}
+
+?>
+
